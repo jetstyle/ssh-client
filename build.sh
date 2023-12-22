d47790d946cc05dba18f187d06b1d80bfcc58de2
Karkh <pavelkarkh@gmail.com>
2023-12-22 13:35:34 +0500

    fix tag

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 docker buildx build --platform linux/amd64 -t ssh-client .
 
-docker tag ssh-client gnextapp/ssh-client:3.18 gnextapp/ssh-client:latest
+docker tag ssh-client gnextapp/ssh-client:3.18
+docker tag ssh-client gnextapp/ssh-client:latest
 
 docker push gnextapp/ssh-client:3.18
 docker push gnextapp/ssh-client:latest
