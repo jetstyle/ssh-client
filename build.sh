750d46ee8b93cf906bdef7031fcbb54b3913ca54
Karkh <pavelkarkh@gmail.com>
2023-12-28 18:05:06 +0500

    fix docker push section

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ docker buildx build --platform linux/amd64 -t $TAG_NAME .
 docker tag $TAG_NAME $ACCOUNT/$TAG_NAME:$VERSION
 docker tag $TAG_NAME $ACCOUNT/$TAG_NAME:latest
 
-docker push $TAG_NAME $ACCOUNT/$TAG_NAME:$VERSION
-docker push $TAG_NAME $ACCOUNT/$TAG_NAME:latest
+docker push $ACCOUNT/$TAG_NAME:$VERSION
+docker push $ACCOUNT/$TAG_NAME:latest
